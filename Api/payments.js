b59dce85e952cedd41f055798fa95da1b8203c58
tarunmali <58786864+tarunmali@users.noreply.github.com>
2023-12-18 14:26:37 +0530

    payments successfull

--- Api/payments.js
+++ Api/payments.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Enrollments = require('../DB/enrollments');
 
 router.post('/', async (req, res) => {
   const { selectedYear, selectedMonth, selectedSlot, email, amount } = req.body;
-    
-  // Implement your actual payment processing logic here
-//   const paymentSuccess = Math.random() < 0.8; // 80% success rate
-  const paymentSuccess = true; // always happpen
-  res.json({ success: paymentSuccess });
+  const slotId = selectedYear + selectedMonth + selectedSlot;
+  // generate random paymentID
+  const paymentId = Math.floor(Math.random() * 10000000000) + 1;
+
+  const newEnrollment = new Enrollments({
+    email: email, // Use the email from the request body
+    slotId: slotId,
+    paymentId: paymentId.toString(), // Convert paymentId to a string
+  });
+
+
+  newEnrollment.save()
+    .then((savedEnrollment) => {
+      res.json({ success: true });
+    })
+    .catch((error) => {
+      res.json({ success: false });
+    });
 });
 
 module.exports = router;
--- Api/signin.js
+++ Api/signin.js
@@ -25,9 +25,9 @@ const {sign}= require('jsonwebtoken')
             else{
                 const accessToken=sign({email:email},"maybegeneraterandomly");
                 var userObj = {
-                    _id: userLogin._id.toString(),
-                    name: userLogin.name,
-                    email: userLogin.email,
+                    // _id: userLogin._id.toString(),
+                    // name: userLogin.name,
+                    // email: userLogin.email,
                     accessToken:accessToken
                     // customText: userLogin.customText
                 } 
