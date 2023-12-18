03096e746969eff3c1256844419d10423f3f665d
tarunmali <58786864+tarunmali@users.noreply.github.com>
2023-12-18 13:41:11 +0530

    loading screen

--- Api/payments.js
+++ Api/payments.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
   const { selectedYear, selectedMonth, selectedSlot, email, amount } = req.body;
-
+    
   // Implement your actual payment processing logic here
 //   const paymentSuccess = Math.random() < 0.8; // 80% success rate
   const paymentSuccess = true; // always happpen
